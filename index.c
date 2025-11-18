4e60cbc99f208ea42d53e23efc80551d81721c3b
povedan0 <guilherme.povedano@estudiante.uam.es>
2025-11-18 20:56:43 +0100

    lectura y escritura de indice en ficheros muchos cambios generales en el módulo index

--- index.c
+++ index.c
@@ -11,15 +11,34 @@ struct _indexbook {
 };
 
 struct _index {
-    IndexBook *books;
+    IndexBook **books; /* pointers to books */
     size_t used;
     size_t size;
 };
 
+IndexBook *book_init(int key, long int offset, size_t size) {
+    IndexBook *book = (IndexBook *) malloc(sizeof(IndexBook));
+    if (book) {
+        book->key = key;
+        book->offset = offset;
+        book->size = size;
+    }
+    return book;
+}
+
+
+void book_free(IndexBook *book) {
+    if (book) free(book);
+}
+
 Index *index_init(void) {
     Index *ind = malloc(sizeof(Index));
     if (ind) {
-        ind->books = (IndexBook *) malloc(INDEX_INIT_SIZE * sizeof(IndexBook));
+        ind->books = (IndexBook **) malloc(INDEX_INIT_SIZE * sizeof(IndexBook *));
+        if (!ind->books) {
+            free(ind);
+            return NULL;
+        }
         ind->size = INDEX_INIT_SIZE;
         ind->used = 0;
     }
@@ -27,9 +46,13 @@ Index *index_init(void) {
 }
 
 void index_free(Index *ind) {
+    size_t i;
     if (ind) 
     {
-        if (ind->books) free(ind->books);
+        if (ind->books) {
+            for (i = 0 ; i < ind->used ; i++) book_free(ind->books[i]);
+            free(ind->books);
+        }
         ind->books = NULL;
         free(ind);
     }
@@ -38,20 +61,26 @@ void index_free(Index *ind) {
 int index_add(Index *ind, IndexBook *book) {
     int m, a = 0, b = ind->used - 1;
 
+    if (!ind || !ind->books || !book) return -1;
+
     if (ind->size == ind->used) /* ensure at least one free space */
     {
-        ind->size *= 2;
-        ind->books = realloc(ind->books, ind->size * sizeof(IndexBook));
-        if (!ind->books) ind->size = 0; return - 1;
+        size_t new_size = ind->size * 2;
+        IndexBook **temp = realloc(ind->books, new_size * sizeof(IndexBook *));
+        if (!temp) {
+            return -1;
+        }
+        ind->books = temp;
+        ind->size = new_size;
     }
 
     while (a <= b) 
     {
-        m = (a + (b - a)) / 2; /* minimize overflow */
+        m = a + (b - a) / 2; /* minimize overflow */
 
-        if (book->key == ind->books[m].key) {
-            return;
-        } else if (book->key < ind->books[m].key) {
+        if (book->key == ind->books[m]->key) {
+            return 0;
+        } else if (book->key < ind->books[m]->key) {
             b = m - 1;
         } else {
             a = m + 1;
@@ -60,15 +89,14 @@ int index_add(Index *ind, IndexBook *book) {
 
     if (ind->used - a > 0) 
     {
-        memmove(&ind->books[a+1], &ind->books[a], (ind->used - a) * sizeof(IndexBook));
+        memmove(&(ind->books[a+1]), &(ind->books[a]), (ind->used - a) * sizeof(IndexBook *));
     }
 
-    ind->books[a] = *book;
+    ind->books[a] = book;
     ind->used++;
 
     return 0;
 }
-
 
 int index_save(Index *ind, char *file) {
     ssize_t k = 0;
@@ -77,14 +105,15 @@ int index_save(Index *ind, char *file) {
 
     if (!ind || !ind->books || !file) return -1;
 
+    /* create temporary file to ensure either all of the index is written or the file stays the same */
     if (!(temp = fopen("save.temp", "wb"))) return -1;
 
     /* WRITE A HEADER FOR THE INDEX FILE (?) */
 
     for (k = 0 ; k < ind->used ; k++) {
-        if (fwrite(&(ind->books[k].key), sizeof(ind->books->key), 1, temp) != 1) goto write_fail;
-        if (fwrite(&(ind->books[k].offset), sizeof(ind->books->offset), 1, temp) != 1) goto write_fail;
-        if (fwrite(&(ind->books[k].size), sizeof(ind->books->size), 1, temp) != 1) goto write_fail;
+        if (fwrite(&(ind->books[k]->key), sizeof(ind->books[k]->key), 1, temp) != 1) goto write_fail;
+        if (fwrite(&(ind->books[k]->offset), sizeof(ind->books[k]->offset), 1, temp) != 1) goto write_fail;
+        if (fwrite(&(ind->books[k]->size), sizeof(ind->books[k]->size), 1, temp) != 1) goto write_fail;
     }
     
     fflush(temp);
@@ -95,7 +124,75 @@ int index_save(Index *ind, char *file) {
 
 write_fail:
     {
-        
+        fclose(temp);
     }
     return -1;
+}
+
+Index *index_init_from_file(char *filename) {
+    Index *index = NULL;
+    FILE *f = NULL;
+    IndexBook *book = NULL;
+
+    if (!filename) return NULL;
+
+    if (!(f = fopen(filename, "rb"))) goto cleanup;
+
+    if (!(index = (Index *) malloc(sizeof(Index)))) goto cleanup;
+
+    if (!(index->books = (IndexBook **) malloc(INDEX_INIT_SIZE * sizeof(IndexBook *)))) goto cleanup;
+
+    index->size = INDEX_INIT_SIZE;
+    index->used = 0;         
+
+    while (1) 
+    {
+        book = NULL;
+        long offset; 
+        size_t size;
+        int key;
+
+        /* expandir array */
+        if (index->size == index->used) {
+            index->size *= 2;
+            index->books = realloc(index->books, index->size * sizeof(IndexBook *));
+            if (!(index->books)) goto cleanup;
+        }
+
+        /* lectura binaria */
+        if (fread(&key, sizeof(key), 1, f) != 1) break; /* fin del fichero */
+        if (fread(&offset, sizeof(offset), 1, f) != 1) goto cleanup; /* lectura parcial - error */
+        if (fread(&size, sizeof(size), 1, f) != 1) goto cleanup;
+
+        /* inicializacion de book struct */
+        book = book_init(key, offset, size);
+        if (!book) goto cleanup;
+
+        /* almacenamiento en index struct */
+        index->books[index->used++] = book;
+    }
+
+    fclose(f);
+
+
+    return index;
+
+cleanup: 
+    {
+        if (f) fclose(f);
+        if (book) book_free(book);
+        if (index) index_free(index);
+    }
+    return NULL;
+}
+
+void index_print(Index *ind) {
+    size_t i;
+    if (!ind || !ind->books) return;
+    for (i = 0 ; i < ind->used ; i++) {
+        fprintf(stdout, "Entry #%ld\n", i);
+        fprintf(stdout, "\tkey: #%d\n", ind->books[i]->key);
+        fprintf(stdout, "\toffset: #%ld\n", ind->books[i]->offset);
+        fprintf(stdout, "\tsize: #%ld\n", ind->books[i]->size);        
+    }   
 }
--- index.h
+++ index.h
@@ -1,12 +1,28 @@
 #ifndef INDEX_H
 #define INDEX_H
 
+#include <stdio.h>
+
 #define INDEX_INIT_SIZE 4
 
 typedef struct _indexbook IndexBook;
 
 typedef struct _index Index;
 
-void index_save();
+IndexBook *book_init(int key, long int offset, size_t size);
+
+void book_free(IndexBook *book);
+
+Index *index_init(void);
+
+void index_free(Index *ind);
+
+int index_add(Index *ind, IndexBook *book);
+
+int index_save(Index *ind, char *file);
+
+Index *index_init_from_file(char *filename);
+
+void index_print(Index *ind);
 
 #endif
