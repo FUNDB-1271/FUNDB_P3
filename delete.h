640c2f8a2dd1032c4990d1dc537b4271fb7f750f
marcomance <marco.mancennido@estudiante.uam.es>
2025-11-23 17:40:53 +0100

    delete.h

--- delete.h
+++ delete.h
@@ -4,3 +4,7 @@ typedef struct _indexdeletedbook IndexDeletedBook;
 
 typedef struct _deletedlist DeletedList;
 
+DeletedList *deletedlist_init();
+void deletedlist_free(DeletedList *deletedlist);
+int deletedlist_add(DeletedList *deletedlist, IndexDeletedBook *indexdeletedbook);
+int deletedlist_update(DeletedList *deletedlist);
